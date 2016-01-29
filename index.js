25e6992f50324454a0699ccd89cc7ea3e1555e05
Alex Banks <alex@moonhighway.com>
2016-01-29 11:15:38 -0800

    Incorporates eslint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*eslint no-console: 0*/
 var server = require('./server');
 process.title = 'reactfluxrecipes';
 server.listen(process.env.PORT || 3000, () => console.log(`Skier Dictionary app running on port 3000`));
